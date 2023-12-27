43bbd3d84cf46a810f469ed1c8df0658b07c0d48
Chandan-Kumar-Mahato <cmhandan@gmail.com>
2023-12-27 18:59:21 +0530

    This is the next level of the course selling app where we use jwt token

--- week-3/02-course-app-easy-2/index.js
+++ week-3/02-course-app-easy-2/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
 
 app.use(express.json());
 
@@ -10,14 +11,43 @@ let COURSES = [];
 // Admin routes
 app.post('/admin/signup', (req, res) => {
   // logic to sign up admin
+  const adminObj = req.body;
+  const dupInd = ADMINS.findIndex((val)=> val.username===adminObj.username);
+  if(dupInd===-1){
+    ADMINS.push(adminObj);
+    const secret = "super30sec"
+    const mssg = jwt.sign(adminObj,secret);
+    res.status(200).json({message:"Admin created successfully , token: " + mssg});
+  }
 });
 
-app.post('/admin/login', (req, res) => {
+function adminAuthentication(req,res,next){
+  const adminLoginObj ={
+    username:req.headers.username,
+    password:req.headers.password
+  };
+  const ind = ADMINS.findIndex((val)=>val.username==adminLoginObj.username && val.password==adminLoginObj.password);
+  if(ind==-1){
+    res.status(401).send("Unauthorized Admin");
+  }
+  else{
+    const secret = "super30sec";
+    const jwtTok = jwt.sign(adminLoginObj,secret);
+    req.token = jwtTok;
+       next();
+  }
+}
+app.post('/admin/login', adminAuthentication , (req, res) => {
   // logic to log in admin
+  res.status(200).json({message:"Admin logged in successfully , token: ",token:req.token
 });
-
+})
 app.post('/admin/courses', (req, res) => {
   // logic to create a course
+  const randomId = Math.floor(Math.random()*100);
+  const courseObj = req.body;
+  courseObj.id = randomId; 
+  COURSES.push(courseObj);
 });
 
 app.put('/admin/courses/:courseId', (req, res) => {
