d8227eff7722cb410c0540ed26e6c490683feddc
Chandan-Kumar-Mahato <cmhandan@gmail.com>
2023-12-28 22:19:15 +0530

    Here this is completed now after using and learning about the jwt

--- week-3/02-course-app-easy-2/index.js
+++ week-3/02-course-app-easy-2/index.js
@@ -52,7 +52,33 @@ function jwtAuthenticate(req,res,next){
       else 
       {
         const checkInd = ADMINS.findIndex((val)=>val.username == mssg.username && val.password == mssg.password);
-        if(checkInd != -1){
+        if(checkInd != -1)
+        {
+          next();
+        }
+      }
+    })
+  }
+  else {
+    res.status(401).send(`Admin auth Failed`);
+  }
+}
+function jwtUserAuthenticate(req,res,next){
+  const tok = req.headers.authorization;
+  if(tok){
+    var msg = tok.split(' ')[1];
+    jwt.verify(msg,secret,(err,mssg)=>{
+      if(err)
+      {
+        res.status(401).send(`Token Auth Failed`);
+      }
+      else 
+      {
+        const checkInd = USERS.findIndex((val)=>val.username == mssg.username && val.password == mssg.password);
+        if(checkInd != -1)
+        {
+          // this is the index of the particular user in the user array
+          req.userInd = checkInd;
           next();
         }
       }
@@ -73,7 +99,7 @@ app.post('/admin/courses', jwtAuthenticate, (req, res) => {
   const courseObj = req.body;
   courseObj.id = randomId; 
   COURSES.push(courseObj);
-  res.send(`Course created successfully`,{courseId:randomId});
+  res.send(`Course created successfully courseId: ${randomId}` );
 });
 
 // this is for editing the particular courses
@@ -98,25 +124,78 @@ app.get('/admin/courses', jwtAuthenticate ,  (req, res) => {
   res.json(COURSES);
 });
 
-// User routes
+// User routes start here
 app.post('/users/signup', (req, res) => {
   // logic to sign up user
+  const obj = req.body;
+  obj.purchaseCourse = [];
+  const findInd = USERS.findIndex((val)=>val.username == obj.username && val.password == obj.password);
+  if(findInd == -1)
+  {
+    USERS.push(obj);
+    const tok = generateToken(obj);
+    res.send({message:"user Created successfully" , token:tok});
+  }
+  else {
+    res.send(`User already crated by that name!!`);
+  }
 });
 
-app.post('/users/login', (req, res) => {
+function userAuthentication(req,res,next){
+  const userObj = {
+    username:req.headers.username,
+    password:req.headers.password
+  };
+
+  const findInd = USERS.findIndex((val)=>val.username == userObj.username && val.password == userObj.password);
+  if(findInd != -1){
+    const tok = generateToken(userObj);
+    req.token = tok;
+    next();
+  }
+  else {
+    res.send(`User from that username Not exist`);
+  }
+}
+app.post('/users/login', userAuthentication,  (req, res) => {
   // logic to log in user
+  res.status(200).send({message:`user loged in successfully `, token:req.token});
 });
 
-app.get('/users/courses', (req, res) => {
+app.get('/users/list',(req,res)=>{
+  res.json(USERS);
+})
+app.get('/users/courses', jwtUserAuthenticate, (req, res) => {
   // logic to list all courses
+  const publishedTrueCourses = [];
+  COURSES.filter((val)=>{
+    if(val.published==true){
+      publishedTrueCourses.push(val);
+    }
+  })
+  res.json(publishedTrueCourses);
+
+  // this must list all the course that are published as true
 });
 
-app.post('/users/courses/:courseId', (req, res) => {
+app.post('/users/courses/:courseId',jwtUserAuthenticate, (req, res) => {
   // logic to purchase a course
+// my first step should be finding the particular user in the users array
+  const courId = req.params.courseId;
+  const purCour = COURSES.find((val)=>val.id==courId);
+  const userind = req.userInd;
+  USERS[userind].purchaseCourse.push(purCour);
+  res.json({message:"course purchased successfully"});
+  // this will purchase the particular course with the course id
 });
 
-app.get('/users/purchasedCourses', (req, res) => {
+app.get('/users/purchasedCourses',jwtUserAuthenticate, (req, res) => {
   // logic to view purchased courses
+  const userind = req.userInd;
+  res.json(USERS[userind].purchaseCourse);
+
+
+  // this will list all the courses that are being puchased
 });
 
 app.listen(3000, () => {
