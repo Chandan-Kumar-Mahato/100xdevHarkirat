44f4b8ba5587fcd655134327916ea46b81b4ef6a
Chandan-Kumar-Mahato <cmhandan@gmail.com>
2024-01-21 21:10:58 +0530

    in this section i learn to connect my program with the mongoose database

--- week-3/04-course-app-hard/index.js
+++ week-3/04-course-app-hard/index.js
@@ -1,15 +1,54 @@
 const express = require('express');
 const app = express();
-
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const secret = 'super30';
 app.use(express.json());
+// lets create a schema 
+const userSchema = new mongoose.Schema({
+  username:{type:String},
+  password:String,
+  purchasedCourses:[{type:mongoose.Schema.Types.ObjectId , ref:'Course'}]
+});
 
-let ADMINS = [];
-let USERS = [];
-let COURSES = [];
+const adminSchema = new mongoose.Schema({
+  username:String , 
+  password:String
+})
+
+const courseSchema = new mongoose.Schema({
+  title:String,
+  description:String,
+  price:Number,
+  published:Boolean
+})
+// lets create a mongoose module
+const Admin = mongoose.model('Admin', adminSchema);
+const  User = mongoose.model('User' , userSchema);
+const Course = mongoose.model('Course' , courseSchema);
+
+// lets connect backend with database
+
+mongoose.connect('mongodb+srv://chandu:9807774772@temprorary.scortpu.mongodb.net/');
+
 
 // Admin routes
-app.post('/admin/signup', (req, res) => {
+app.post('/admin/signup', async(req, res) => {
   // logic to sign up admin
+  const {username , password} = req.body;
+  const admin = await Admin.findOne({username});
+  if(admin){
+    res.status(403).json({message:"Admin already Exists"});
+  }
+  else 
+  {
+  const obj  = {username , password};
+  const newAdmin = new Admin(obj);
+  newAdmin.save();
+  const token = jwt.sign(obj , secret , {expiresIn:'1h'});
+    res.json({message:"Admin created successfully" , token});
+}
+  
 });
 
 app.post('/admin/login', (req, res) => {
