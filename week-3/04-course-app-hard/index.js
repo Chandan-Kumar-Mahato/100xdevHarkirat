911b9709a046cedff1cd8ff401d84c6ebf83d8f7
chandu <cmhandan@gmail.com>
2024-04-03 20:38:31 +0530

    Backend of course selling app with mongo db

--- week-3/04-course-app-hard/index.js
+++ week-3/04-course-app-hard/index.js
@@ -1,116 +1,204 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
-const secret = 'super30';
+const jwt = require("jsonwebtoken");
+const secret = "super30";
+const { Admin, User, Course } = require("./model/model.js");
+const { connectDB } = require("./model/dbConn.js");
 app.use(express.json());
-// lets create a schema 
-const userSchema = new mongoose.Schema({
-  username:{type:String},
-  password:String,
-  purchasedCourses:[{type:mongoose.Schema.Types.ObjectId , ref:'Course'}]
-});
-
-const adminSchema = new mongoose.Schema({
-  username:String , 
-  password:String
-})
-
-const courseSchema = new mongoose.Schema({
-  title:String,
-  description:String,
-  price:Number,
-  published:Boolean
-})
-// lets create a mongoose module
-const Admin = mongoose.model('Admin', adminSchema);
-const  User = mongoose.model('User' , userSchema);
-const Course = mongoose.model('Course' , courseSchema);
-
-// lets connect backend with database
-
-mongoose.connect('mongodb+srv://chandu:9807774772@temprorary.scortpu.mongodb.net/');
-
+connectDB();
 
 // Admin routes
-app.post('/admin/signup', async(req, res) => {
+app.post("/admin/signup", async (req, res) => {
   // logic to sign up admin
-  const {username , password} = req.body;
+  const { username, password } = req.body;
 
   // Admin.findOne is one of the asynchronous so , we use async and await
-  const admin = await Admin.findOne({username});
-  if(admin){
-    res.status(403).json({message:"Admin already Exists"});
-  }
-  else 
-  {
+  const admin = await Admin.findOne({ username });
+  if (admin) {
+    res.status(403).json({ message: "Admin already Exists" });
+  } else {
     // if you got key and value named same you can use any of then only once
-  const obj  = {username , password};
-  const newAdmin = new Admin(obj);
-  newAdmin.save();
-  const token = jwt.sign(obj , secret , {expiresIn:'1h'});
-    res.json({message:"Admin created successfully" , token});
-}
-  
-});
-
-app.post('/admin/login', (req, res) => {
-  // logic to log in admin
-  const {username , password} = req.headers;
-  const admin = Admin.findOne({username , password});
-  if(admin)
-  {
-    const token = jwt.sign({username , password} , secret , {expiresIn:"1h"});
-    res.json({message:"Admin logged in successfully" , Token:token});
+    const obj = { username, password };
+    const newAdmin = new Admin(obj);
+    newAdmin.save();
+    const token = jwt.sign(obj, secret, { expiresIn: "1h" });
+    res.json({ message: "Admin created successfully", token });
   }
-  else 
-  res.status(403).json({message:"Invalid username or password"});
-
 });
-function authenticateJwt(req,res,next)
-{
-  var  auth = req.headers.authorization;
-   auth = auth.split(' ')[1];
-   console.log(auth);
-   jwt.verify(auth,secret,(err,data)=>{
-   console.log(data);
-   next();
-   })
-}
-app.post('/admin/courses', authenticateJwt ,  (req, res) => {
+
+app.post("/admin/login", async (req, res) => {
+  // logic to log in admin
+  try {
+    const { username, password } = req.headers;
+    const admin = await Admin.findOne({ username, password });
+    if (admin) {
+      const token = jwt.sign({ username, password }, secret, {
+        expiresIn: "1h",
+      });
+      res.json({ message: "Admin logged in successfully", Token: token });
+    } else {
+      res.send("admin not found");
+    }
+  } catch (error) {
+    res.status(403).json({ message: "Invalid username or password" });
+  }
+});
+
+const adminAuthenticateJwt = async (req, res, next) => {
+  try {
+    let authorization = req.headers.authorization.split(" ")[1];
+    const { username, password } = jwt.verify(authorization, secret);
+    const admin = await Admin.findOne({ username, password });
+    if (admin) {
+      next();
+    } else {
+      res.status(404).send(`Admin Not found`);
+    }
+  } catch (error) {
+    if (error instanceof jwt.TokenExpiredError) {
+      res.status(500).send({ message: "Token expired" });
+    } else if (error instanceof jwt.JsonWebTokenError) {
+      res.status(500).send({ message: "admin auth failed" });
+    }
+  }
+};
+const userAuthenticateJwt = async (req, res, next) => {
+  try {
+    let authorization = req.headers.authorization.split(" ")[1];
+    const { username, password } = jwt.verify(authorization, secret);
+    const user = await User.findOne({ username, password });
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      res.status(404).send(`User Not found`);
+    }
+  } catch (error) {
+    if (error instanceof jwt.TokenExpiredError) {
+      res.status(500).send({ message: "Token expired" });
+    } else if (error instanceof jwt.JsonWebTokenError) {
+      res.status(500).send({ message: "User auth failed" });
+    }
+  }
+};
+
+// { message: 'Course created successfully', courseId: 1 }
+app.post("/admin/courses", adminAuthenticateJwt, (req, res) => {
   // logic to create a course
-  res.send('Called Successfully');
+  const courseObj = req.body;
+  const id = Math.floor(Math.random() * 1000);
+  courseObj.id = id;
+  const newCourse = new Course(courseObj);
+  newCourse.save();
+  res.json({ message: "Course Created successfully", id });
 });
 
-app.put('/admin/courses/:courseId', (req, res) => {
+app.put("/admin/courses/:courseId", adminAuthenticateJwt, async (req, res) => {
   // logic to edit a course
+  try {
+    const updatedCourse = req.body;
+    const { courseId } = req.params;
+    updatedCourse.id = courseId;
+    const id = parseInt(courseId);
+    const course = await Course.findOneAndUpdate({ id }, updatedCourse, {
+      new: true,
+    });
+    if (course) {
+      res.send({ message: `Course updated`, courseId });
+    } else {
+      res.send(`course not found`);
+    }
+  } catch (error) {
+    console.log(error);
+    res.send(`Error occured`);
+  }
 });
 
-app.get('/admin/courses', (req, res) => {
+app.get("/admin/courses", adminAuthenticateJwt, async (req, res) => {
   // logic to get all courses
+  try {
+    const course = await Course.find({});
+    res.send(course);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
 });
 
-// User routes
-app.post('/users/signup', (req, res) => {
+// User routes started
+app.post("/users/signup", async (req, res) => {
   // logic to sign up user
+  const { username, password } = req.body;
+
+  // Admin.findOne is one of the asynchronous so , we use async and await
+  const admin = await User.findOne({ username });
+  if (admin) {
+    res.status(403).json({ message: "User already Exists" });
+  } else {
+    // if you got key and value named same you can use any of then only once
+    const obj = { username, password };
+    const newUser = new User(obj);
+    newUser.save();
+    const token = jwt.sign(obj, secret, { expiresIn: "1h" });
+    res.json({ message: "User created successfully", token });
+  }
 });
 
-app.post('/users/login', (req, res) => {
+app.post("/users/login", async (req, res) => {
   // logic to log in user
+  try {
+    const { username, password } = req.headers;
+    console.log(username, password);
+    const user = await User.findOne({ username, password });
+    if (user) {
+      const token = jwt.sign({ username, password }, secret, {
+        expiresIn: "1h",
+      });
+      res.json({ message: "User logged in successfully", Token: token });
+    } else {
+      res.send("User not found");
+    }
+  } catch (error) {
+    res.status(403).json({ message: "Invalid username or password" });
+  }
 });
 
-app.get('/users/courses', (req, res) => {
-  // logic to list all courses
+app.get("/users/courses", userAuthenticateJwt, async (req, res) => {
+  // logic to list all courses\
+  try {
+    const course = await Course.find({ published: true });
+    console.log(course);
+
+    res.send(course);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
-app.post('/users/courses/:courseId', (req, res) => {
+app.post("/users/courses/:courseId", userAuthenticateJwt, async (req, res) => {
   // logic to purchase a course
+  try {
+    const { courseId } = req.params;
+    const { _id } = await Course.findOne({ id: courseId });
+    req.user.purchasedCourses.push(_id);
+    req.user.save();
+    res.send(`course bought successfully`);
+  } catch (error) {
+    res.send("error");
+  }
 });
 
-app.get('/users/purchasedCourses', (req, res) => {
+app.get("/users/purchasedCourses", userAuthenticateJwt, async(req, res) => {
+  try {
+    await req.user.populate("purchasedCourses");
+    console.log(req.user.purchasedCourses);
+    res.send("get course");
+  } catch (error) {
+    res.send(error);
+  }
   // logic to view purchased courses
 });
 
 app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+  console.log("Server is listening on port 3000");
 });
